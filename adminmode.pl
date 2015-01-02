d1a68b0f59c6ae1288be1cb1956017b7f1166754
Jeebeevee <jelmer@jeebeevee.nl>
2015-01-02 16:00:41 +0100

    version 1

--- adminmode.pl
+++ adminmode.pl
@@ -4,6 +4,35 @@
 # Version:	0.1
 # Author:	Jeebeevee
 # Date:		2015-12-29
-# Description:	opens a query tab
+# Description:	opens a query tab and place notice send to the user in this tab.
 #
 
+# Basic load 
+use strict;
+use warnings;
+use Xchat qw( :all );
+
+# version
+my $NAME    = 'UserIRC-admin (he)xChat';
+my $VERSION = '0.1';
+
+register($NAME, $VERSION, 'UserIRC-admin (he)xChat ontwikkeld door Jeebeevee voor Scoutlink');
+prnt("Geladen $NAME $VERSION");
+
+# Start script 
+admin_startup();
+
+#use hook
+foreach ('Channel Notice', 'Notice') {
+        hook_print($_, \&getNotice);
+}
+
+# get the notice to the tab
+sub getNotice {
+	
+}
+
+#open tab for notice
+sub admin_startup {
+	Xchat::command("query Usermonitor");
+}
